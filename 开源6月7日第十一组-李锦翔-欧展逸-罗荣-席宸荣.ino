60b87783820104d9851fd7ccef23f6bf49452d9f
ljx512666 <105158931+ljx512666@users.noreply.github.com>
2022-06-07 22:22:24 +0800

    Add files via upload

---
+++ 开源6月7日第十一组-李锦翔-欧展逸-罗荣-席宸荣.ino
@@ -0,0 +1,12 @@
+#include <Breath.h>
+
+Breath pin(11);
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pin.breath(ON);
+}
